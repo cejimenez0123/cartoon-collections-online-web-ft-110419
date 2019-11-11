941c8fb0f5d0e1a3d7f3f8989df7a9550abc1236
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-11 22:53:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(snack)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if snack.include?(cheese_types)
-    return cheese_types
+    return cheese_types[]
   end
 
 end
