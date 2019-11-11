bd7d0aad2157e9afc32d6e15766e2d0c82f1aa1e
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-11 22:55:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(snack)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   snack.find?(cheese_types)
-    
-  
+
+
 
 end
